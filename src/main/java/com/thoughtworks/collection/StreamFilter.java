7a64b391e8d5d6faa278a1c68b532fa34246be15
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-04 12:22:14 +0800

    [JAVAStream][Gerarld] feat: Java Stream Filter Exercise

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -3,19 +3,35 @@ package com.thoughtworks.collection;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        throw new NotImplementedException();
+
+        return numbers.stream().filter(number -> number % 2 == 0).collect(Collectors.toList());
+
+        //throw new NotImplementedException();
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        throw new NotImplementedException();
+
+        return words
+                .stream()
+                .filter(word -> word.endsWith("s") ||  word.endsWith("S"))
+                .collect(Collectors.toList());
+        //throw new NotImplementedException();
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
-        throw new NotImplementedException();
+
+
+        return numbers
+                .stream()
+                .filter( number -> anotherNumbers.contains(number) )
+                .collect(Collectors.toList());
+
+        //throw new NotImplementedException();
     }
 
 
